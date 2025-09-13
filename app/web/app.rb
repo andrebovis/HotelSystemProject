6d76635019a61aeef422e3786b88c60732806ed2
andre bovis <andre_bovis@hotmail.com>
2025-09-13 03:17:53 -0300

    new index layout with only reservations showing on client view

--- app/web/app.rb
+++ app/web/app.rb
@@ -6,30 +6,63 @@ require_relative '../models/quarto'
 require_relative '../models/reserva'
 
 class HotelApp < Sinatra::Base
-  # configurações corretas dentro da classe
+  # configurações
   set :public_folder, File.expand_path('../../public', __dir__)
   set :views, File.expand_path('../views', __dir__)
 
-  # rota inicial
+  # rota inicial (página principal)
   get '/' do
     erb :index
   end
 
+  # lista clientes (somente lado do hotel)
   get '/clientes' do
     db = create_client
     @clientes = db.query("SELECT * FROM clientes")
     erb :clientes
   end
 
+  # lista quartos
   get '/quartos' do
     db = create_client
     @quartos = db.query("SELECT * FROM quartos")
     erb :quartos
   end
 
+  # lista reservas
   get '/reservas' do
     db = create_client
     @reservas = db.query("SELECT * FROM reservas")
     erb :reservas
   end
+
+  # formulário para nova reserva
+  get '/reserva/nova' do
+    db = create_client
+    @quartos = db.query("SELECT * FROM quartos")
+    erb :nova_reserva
+  end
+
+  # processa a reserva (quando o form for enviado)
+  post '/reserva' do
+    db = create_client
+
+    cliente_nome  = params[:nome]
+    data_checkin  = params[:checkin]
+    data_checkout = params[:checkout]
+    quarto_id     = params[:quarto_id]
+    pagamento     = params[:pagamento]
+
+    # cria cliente
+    db.query("INSERT INTO clientes (nome) VALUES ('#{cliente_nome}')")
+
+    # cria reserva vinculada ao cliente
+    db.query("INSERT INTO reservas (cliente_id, quarto_id, data_checkin, data_checkout, metodo_pagamento)
+              VALUES (LAST_INSERT_ID(), #{quarto_id}, '#{data_checkin}', '#{data_checkout}', '#{pagamento}')")
+
+    erb :reserva_confirmada
+  end
 end
+
+# executa o app
+HotelApp.run! if __FILE__ == $0
